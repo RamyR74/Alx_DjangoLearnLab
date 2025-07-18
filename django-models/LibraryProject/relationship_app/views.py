d6baa27e337f1bac2408d5d8212ac46c3862d1da
RamyR74 <ramycourses74@gmail.com>
2025-07-18 14:17:46 +0300

    Fix: cleaned imports and ensured Library model is imported

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
+
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
