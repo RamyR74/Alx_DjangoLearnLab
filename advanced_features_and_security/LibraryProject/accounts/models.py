d87d98649e846853575b229cb8fe0e7fd9c46196
RamyR74 <ramycourses74@gmail.com>
2025-07-26 12:02:04 +0300

    Clean up: Removed duplicate CustomUser from bookshelf, cleared migrations and db

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    birth_date = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
